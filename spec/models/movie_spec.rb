b6dfd6f70319f541640544c3f88dca3f551b323f
Arman Jon Villalobos <me@armanjon.com>
2016-05-22 22:46:06 +0800

    added model skeleton test for movie

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Movie do
+  it "is valid with a title, released date, description, total gross, image filename and rating" do
+
+  end
+
+  it "is invalid without a title" do
+
+  end
+
+  it "is invalid without a released date" do
+
+  end
+
+  it "is invalid when the description is less than 25 characters" do
+    
+  end
+
+  it "is invalid when the total gross is less than 0" do
+
+  end
+
+  it "is invalid when image filename is not either GIF, PNG, or JPG" do
+
+  end
+
+  it "is invalid when the rating is not from the accepted options" do
+
+  end
+end
