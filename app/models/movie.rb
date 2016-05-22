148dddbbb8055e78a2ab59527d67fb45cc8dd4d1
Arman Jon Villalobos <me@armanjon.com>
2016-05-22 20:48:11 +0800

    added validation for movie resource

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :released_on, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+    with: /\w+.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+  RATINGS = %w(G PG PG-13 R NC-17)
+  validates :rating, inclusion: { in: RATINGS }
 end
